eb226bb706ec8ad2fbfc2f1035f2f82cb27e31e3
Emeline Niyogisubizo <rubayizaniyogisubizoemeline@gmail.com>
2025-08-31 18:10:15 +0300

    added files

--- moviewebnextjs/src/app/sharedcomponents/NavBar/index.tsx
+++ moviewebnextjs/src/app/sharedcomponents/NavBar/index.tsx
@@ -1,34 +1,3 @@
-
-// import Link from "next/link";
-
-// const NavBar = () => {
-//   return (
-//     <div>
-//       <header className="flex justify-between items-center p-4 border-b border-gray-800">
-//         <div className="flex items-center">
-//           <h1 className="text-2xl font-bold text-red-500">MovieHub 🎬</h1>
-//           <input
-//             type="text"
-//             placeholder="Search..."
-//             className="ml-4 w-64 px-4 py-2 bg-gray-800 text-white rounded-full focus:outline-none"
-//           />
-//         </div>
-//         <nav className="flex space-x-4">
-//           <a href="#" className="text-red-500 hover:underline">Home</a>
-//           <Link href="/mylist" className="text-red-500 hover:underline top-1">
-//             My List
-//           </Link>
-//           <a href="/login">
-//             <button className="bg-red-500 text-black px-4 py-2 rounded-[14px]">Sign In</button>
-//           </a>
-//         </nav>
-//       </header>
-//     </div>
-//   )
-// }
-
-// export default NavBar;
-
 "use client";
 import Link from "next/link";
 
